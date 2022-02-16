26d162218f895bc65d445ababa38a468d036d5b2
Danilo <dspinerosp@gmail.com>
2022-02-16 12:26:27 -0500

    Crear actualización gráfica participacion-nsv

--- dashboard.py
+++ dashboard.py
@@ -4,16 +4,17 @@ from dash import html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import pandas as pd
-from obtener_datos import CONDICIONES
+# from obtener_datos import CONDICIONES
 from datetime import datetime as dt
 from datetime import date, timedelta
+import numpy as np
 
-from procesar_datos import total_crudo, total_crudo_detallado, total_crudo_empresa
+from procesar_datos import filtrar_datos_fechas, nsv_despacho_remitente, nsv_recibo_remitente, total_crudo, total_crudo_detallado, total_crudo_empresa
 
 # Cargos los datos del balance
 datos = pd.read_csv('balance.csv')
 # Convertir las fechas al tipo de dato datatime
-datos['fecha'] = pd.to_datetime(datos['fecha'], dayfirst=True)
+datos['fecha'] = pd.to_datetime(datos['fecha'], format='%d-%m-%Y')
 
 # Calcular los acumulados por tipo de operación de las condiciones de operación para NSV
 total_entrega = total_crudo(datos, 'ENTREGA')['NSV']
@@ -56,23 +57,6 @@ app.layout = html.Div([
         ], className="one-third column", id='title1'),
 
     ], id="header", className="row flex-display", style={"margin-bottom": "25px"}),
-    html.Div([
-        html.Div([
-            html.H6('Periodo de Análisis', style={'color':'white'}),
-            # Permite seleccionar las fechas en las que se quiere realizar el análisis
-            dcc.DatePickerRange(
-                id='periodo-analisis',
-                # Las fechas mínimas y máximas permitidas dependerán de las fechas de los datos del balance
-                min_date_allowed=datos['fecha'].min().to_pydatetime(),
-                max_date_allowed=datos['fecha'].max().to_pydatetime(),
-                initial_visible_month=dt(datos['fecha'].dt.year.max(), datos['fecha'].max().to_pydatetime().month, 1),
-                # Por defecto toma como periodo de análisis los datos recolectados del último mes.
-                start_date=(datos[datos['fecha'].dt.month == dt.today().month]['fecha'].min()).to_pydatetime(),
-                end_date=datos['fecha'].max().to_pydatetime(),
-            ),
-            html.Div(id='salida-periodo-analisis', style={'color':'white'})
-        ], className='one-third column')
-    ], id="filtro-fechas", className="row flex-display"),
     # Contenedor donde se ubicaran los 4 principales datos acumulados del dashboard
     html.Div([
         # Contenedor para las entregas acumuladas de NSV para todas las empresas
@@ -83,7 +67,7 @@ app.layout = html.Div([
                         'color': 'white'}
                     ),
 
-            html.P(f"{total_entrega:,.2f}", # formatea el valor a 3 decimales
+            html.P(f"{total_entrega:,.2f}", # formatea el valor a 2 decimales
                    style={
                        'textAlign': 'center',
                        'color': 'orange',
@@ -136,22 +120,45 @@ app.layout = html.Div([
                    )], className="card_container three columns")
 
     ], className="row flex-display"),
+    # Contenedor para crear los dos filtros (periodo de análisis y tipo de operación)
     html.Div([
-        # Contenedor para crear el filtro por condiciones de operación y mostrar los resultados del último día
+        # Contenedor para la selección del período de análisis
         html.Div([
-            # Filtrar datos según las condicione de operación
-            html.P('Condiciones de Operación:', className='fix_label', style={'color':'white'}),
-            dcc.Dropdown(options=CONDICIONES,
-                        value='NSV',
+            html.P('Periodo de Análisis', className='fix_label', style={'color':'white'}),
+            # Permite seleccionar las fechas en las que se quiere realizar el análisis
+            dcc.DatePickerRange(
+                id='periodo-analisis',
+                # Las fechas mínimas y máximas permitidas dependerán de las fechas de los datos del balance
+                min_date_allowed=datos['fecha'].min().to_pydatetime(),
+                max_date_allowed=datos['fecha'].max().to_pydatetime(),
+                initial_visible_month=dt(datos['fecha'].dt.year.max(), datos['fecha'].max().to_pydatetime().month, 1),
+                # Por defecto toma como periodo de análisis los datos recolectados del último mes.
+                start_date=(datos[datos['fecha'].dt.month == dt.today().month]['fecha'].min()).to_pydatetime(),
+                end_date=datos['fecha'].max().to_pydatetime(),
+                display_format='DD/MM/Y'
+            ),
+        ], className='create_container four columns'),
+        # Contenedor para la selección del tipo de operación a analizar
+        html.Div([
+            # Filtrar datos según el tipo de operación (entrega, recibo, despacho)
+            html.P('Tipo de Operación a analizar', className='fix_label', style={'color':'white'}),
+            dcc.Dropdown(options=datos['operacion'].unique(),
+                        value='RECIBO POR REMITENTE TIGANA',
                         clearable=False,
-                        id='cond_operacion',
-                        multi=False),
-            html.P(f"Resultados Operación: {datos['fecha'].iloc[-1].strftime('%d/%m/%Y')}",
-                    className='fix_label', style={'color':'white', 'text-align':'center'})
-        ], className='create_container three columns', id='cross-filter-options'), 
-        # Contenedor para graficar la participación en la producción por empresa (según la condición elegida)
+                        id='tipo-operacion',
+                        multi=False)
+        ], className='create_container eight columns', id='cross-filter-options')
+    ], id="filtro-analisis", className="row flex-display"),
+    html.Div([
+        # Contenedor para mostrar los resultado de la operación de la fecha más reciente de actualización
+        # para Geopark
         html.Div([
-            dcc.Graph(id='pie_chart',
+            html.P(f"Resultados Operación: {datos['fecha'].iloc[-1].strftime('%d/%m/%Y')}",
+                className='fix_label', style={'color':'white', 'text-align':'center'})
+        ], className='create_container three columns'), 
+        # Contenedor para graficar la participación en la producción por empresa (según la operación elegida)
+        html.Div([
+            dcc.Graph(id='participacion-empresa',
                     config={'displayModeBar':'hover'})
         ], className='create_container four columns'),
         # Contenedor para graficar la producción histórica por tipo de empresa y condición de operación
@@ -170,44 +177,54 @@ app.layout = html.Div([
     ], className='row flex-display')
 ], id='mainContainer', style={'display':'flex', 'flex-direction':'column'})
 
-#@app.callback(Output('salida-periodo-analisis', 'children'),
-#        [Input('periodo-analisis', 'start_date'),
-#        Input('periodo-analisis', 'end_date')]
-#)
-#def actualizar_periodo(start_date, end_date):
-#    string_prefix = 'Has Seleccionado como '
-#    if start_date is not None:
-#        start_date = dt.strptime(start_date, '%Y-%m-%d')
-#        start_date_string = start_date.strftime('%d/%m/%Y')
-#        string_prefix = string_prefix + f'fecha inicial el {start_date_string} y '
-#    if end_date is not None:
-#        end_date = dt.strptime(end_date, '%Y-%m-%d')
-#        end_date_string = end_date.strftime('%d/%m/%Y')
-#        days_selected = (end_date - start_date).days
-#        string_prefix = string_prefix + 'fecha final el ' + end_date_string + ', para un total de ' + str(days_selected + 1) + ' días.'
-#    if len(string_prefix) == len('You have selected: '):
-#        return 'Select a date to see it displayed here'
-#    else:
-#        return string_prefix
-
-@app.callback(Output('bar_chart1', 'figure'),
-            [Input('cond_operacion', 'value')]
+# Callback para actualizar la gráfica de participación de la empresa
+@app.callback(Output('participacion-empresa', component_property='figure'),
+            [Input('periodo-analisis', 'start_date'),
+            Input('periodo-analisis', 'end_date'),
+            Input('tipo-operacion', 'value')]
 )
-def actualizar_bar(cond_operacion):
-    datos_filtrados = total_crudo_detallado(datos, 'RECIBO')[cond_operacion]
-    traces = []
-    for empresa in datos_filtrados.index.values:
-        datos_empresa = datos_filtrados.loc[empresa, :]
-        traces.append(go.Bar(
-                        name=empresa, 
-                        x=datos_filtrados.columns.values, 
-                        y=datos_empresa,
-        ))
-    return {'data':traces, 
-            'layout':go.Layout(title='My plot',
-                                xaxis={'title':'Campo'},
-                                yaxis={'title':f'{cond_operacion} (bbls)'},
-                                barmode='stack')}
+def actualizar_participacion(start_date, end_date, value):
+    datos_filtrados = filtrar_datos_fechas(datos, start_date, end_date)
+    # datos_filtrados = datos_filtrados[datos_filtrados['operacion'] == value]
+    if 'RECIBO' in value:
+        datos_filtrados = nsv_recibo_remitente(datos_filtrados)
+    elif 'DESPACHO' in value:
+        datos_filtrados = nsv_despacho_remitente(datos_filtrados)
+    else:
+        # Espacio para crear el filtro de 'ENTREGA'
+        pass
+    trace =[go.Pie(labels=datos_filtrados.columns.values,
+                            values=[np.sum(datos_filtrados[empresa]) for empresa in datos_filtrados.columns],
+                            hoverinfo='percent',
+                            textinfo='label+value',
+                            textfont=dict(size=13),
+                            hole=.5,
+                            rotation=45,
+                            textposition='outside')]
+    layout = go.Layout(
+        plot_bgcolor='#1f2c56',
+            paper_bgcolor='#1f2c56',
+            hovermode='closest',
+            title={
+                'text': 'Participación NSV',
+                'y': 0.93,
+                'x': 0.5,
+                'xanchor': 'center',
+                'yanchor': 'top'},
+            titlefont={
+                       'color': 'white',
+                       'size': 25},
+            legend={
+                'orientation': 'h',
+                'bgcolor': '#1f2c56',
+                'xanchor': 'center', 'x': 0.5, 'y': -0.07},
+            font=dict(
+                family="sans-serif",
+                size=12,
+                color='white'),
+            margin=dict(t=60, b=30, l=30, r=30)
+            )
+    return {'data':trace, 'layout':layout}
 
 if __name__ == '__main__':
     app.run_server(debug=True)
