3868673400e20b0828ef2d46a9f7ccfb8d8acbd5
Danilo <dspinerosp@gmail.com>
2022-02-17 20:18:27 -0500

    Cambiar algunas funciones para calcular crudo
    Se eliminaron las funciones nsv_despacho_remitente y nsv_recibo_remitente
    y se creó una función más genérica llamada crudo_operacion_remitente,
    la cual recibo tres parámetros, datos, crudo, operación y calcula
    el total diario por el tipo de crudo y la operación especificados.

--- procesar_datos.py
+++ procesar_datos.py
@@ -14,9 +14,9 @@ def leer_datos(documento):
     df = pd.read_csv(documento)
     return df
 
-def nsv_recibo_remitente(datos):
+def crudo_operacion_remitente(datos, cond_operacion, tipo_operacion):
     """
-    Retorna un DataFrame con el total de NSV recibido diario por cada remitente.
+    Retorna un DataFrame con el total por tipo de crudo de determinado operación diario por cada remitente.
 
     Parámetros:
     -----------
@@ -27,8 +27,8 @@ def nsv_recibo_remitente(datos):
     -> DataFrame - DataFrame que contiene el total diario de NSV por cada empresa, una columna contiene los resultados de una empresa.
     """
     # Filtrar solo los datos cuya operación es un recibo
-    recibidos = datos[['RECIBO' in fila for fila in datos['operacion']]]
-    return recibidos.groupby(['fecha', 'empresa'])['NSV'].sum().unstack()
+    recibidos = datos[[tipo_operacion in fila for fila in datos['operacion']]]
+    return recibidos.groupby(['fecha', 'empresa'])[cond_operacion].sum().unstack()
 
 def total_crudo_detallado(datos, tipo_operacion):
     """
@@ -105,27 +105,6 @@ def total_nsv_recibo(datos):
         total += total_nsv[empresa] # Acumular el total de cada empresa
     total_nsv['TOTAL'] = total # Agregar al diccionario el total de NSV
     return total_nsv 
-
-def nsv_despacho_remitente(datos):
-    """
-    Retorna un DataFrame con el total de NSV daspachado diariamente por cada remitente.
-
-    Parámetros:
-    -----------
-    datos -> DataFrame - Contiene los datos del balance que serán usados para calcular el todal de NSV despachado diario por cada empresa.
-
-    Retorna:
-    -------
-    -> DataFrame - DataFrame que contiene el total diario de NSV por cada empresa, una columna contiene los resultados de una empresa.
-    """
-    # Filtrar unicamente los datos cuya operación es un despacho
-    despachos = datos[['DESPACHO' in fila for fila in datos['operacion']]]
-    # Filtrar los datos que no contienen Tigana y Jacana
-    despachos = despachos[(despachos['campo'] != 'TIGANA ESTACION') & (despachos['campo'] != 'JACANA ESTACION')]
-    # Calcular despachos de NSV diario por empresa
-    despachos = despachos.groupby(['fecha', 'empresa'])['NSV'].sum().unstack()
-    # Se retorna el DataFrame con los datos calculados, y se remplazan los valores NaN por 0.
-    return despachos.fillna(0)
 
 def filtrar_datos_fechas(datos, inicio, fin):
     """
