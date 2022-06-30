f77646c6ce1dc648f344c4ab2f251bf038b4d23f
Danilo Piñeros <danilo_pineros@citecsolutions.com>
2022-06-30 00:37:46 -0500

    Drop rown with invalid values

--- components/table.py
+++ components/table.py
@@ -1,4 +1,8 @@
+from pydoc import classname
 from dash import dash_table
 
 def make_dash_table(df):
-    return dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns], id='tbl')
+    df = df.dropna(axis=0)
+    return dash_table.DataTable(df.to_dict('records'), 
+                            [{"name": i, "id": i} for i in df.columns], 
+                            id='tbl')
