e8aaaa3db0a813ea9c2dac7eba9d904761ef232d
Danilo Piñeros <danilo_pineros@citecsolutions.com>
2022-07-14 10:03:37 -0500

    Add DatePickerRange instead of dropdown with months for filter nominations data

--- pages/nominations/nominations.py
+++ pages/nominations/nominations.py
@@ -6,8 +6,11 @@ from dash import (Dash,
                 html, 
                 dcc)
 import pandas as pd
+from datetime import datetime as dt
 
 from components.button import make_dash_button
+
+from data.server import datos
 
 layout = html.Div([
     html.Div([
@@ -15,30 +18,38 @@ layout = html.Div([
         make_dash_button("Descargar Info Nominaciones", type_button="download")
     ], className='button-container'),
     html.Div([
-        html.H2("Mes"),
-        dcc.Dropdown(options=['Enero', 'Febrero', 'Marzo', 'Abril'],
-                        value='Abril', 
+        html.Div([
+            html.H2("Periodo de Análisis"),
+            dcc.DatePickerRange(
+                id='nomination-period',
+                # Las fechas mínimas y máximas permitidas dependerán de las fechas
+                # de los datos del balance
+                min_date_allowed=datos['fecha'].min().to_pydatetime(),
+                max_date_allowed=datos['fecha'].max().to_pydatetime(),
+                initial_visible_month=dt(datos['fecha'].dt.year.max(),
+                                        datos['fecha'].max().to_pydatetime().month, 1),
+                # Por defecto toma como periodo de análisis los datos recolectados del último mes.
+                start_date=(datos[datos['fecha'].dt.month == datos['fecha'].max().month]['fecha'].min()).to_pydatetime(),
+                end_date=datos['fecha'].max().to_pydatetime(),
+                display_format='DD/MM/Y'),
+            html.H2("Remitente"),
+            dcc.Dropdown(options=['GEOPARK', 'VERANO'],
+                        value='GEOPARK', 
                         clearable=False,
-                        id='nomination-month',
+                        id='remitente-nominacion',
                         multi=False),
-    ]),
-    html.Div([
+            ], className="create_container three columns"),
+        html.Div([
         dcc.Tabs(id="tabs-nominations", value='tigana', 
             children=[dcc.Tab(label='Tigana', value='tigana'),
                     dcc.Tab(label='Livianos', value='livianos'),
             ]
         ),
         dcc.Graph(id="graph-nominations-results")
-    ], className="tabs-container"),
+        ], className="create_container nine columns"),
+    ], className="row flex-display"),
         # Filtrar datos según el tipo de operación (entrega, recibo, despacho)
     html.Div([
-            html.H2("Remitente",
-                    className='fix_label'),
-            dcc.Dropdown(options=['GEOPARK', 'VERANO'],
-                        value='GEOPARK', 
-                        clearable=False,
-                        id='remitente-nominacion',
-                        multi=False),
             dcc.Graph(id="production-factor"),
         ], className="create_container twelve columns"),
     # Contenedor para generar el filtro sobre el tipo de crudo
