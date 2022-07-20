c681da95ee78225ff5ed0796f5b482c257f26f2d
Danilo Piñeros <danilo_pineros@citecsolutions.com>
2022-07-20 18:47:47 -0500

    Delete server.py file

--- data/server.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from utils.constants import balance_data
-
-
-# Cargos los datos del balance
-datos = pd.read_csv(balance_data)
-# Convertir las fechas al tipo de dato datatime
-datos['fecha'] = pd.to_datetime(datos['fecha'], format='%d-%m-%Y')
