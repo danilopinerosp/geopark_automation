384a977789aaa471f41d22e538bf8bd7b608211f
Danilo Piñeros <danilo_pineros@citecsolutions.com>
2022-07-31 21:04:31 -0500

    Remove setting for development environment

--- environment/settings.py
+++ environment/settings.py
@@ -2,14 +2,14 @@
 The module creates the logic to read different configurations based on the environment
 """
 
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
+#import os
+#from os.path import join, dirname
+#from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), os.getenv("ENVIRONMENT_FILE"))
-load_dotenv(dotenv_path=dotenv_path, override=True)
+#dotenv_path = join(dirname(__file__), os.getenv("ENVIRONMENT_FILE"))
+#load_dotenv(dotenv_path=dotenv_path, override=True)
 
-APP_HOST = os.environ.get("HOST")
-APP_PORT = int(os.environ.get("PORT"))
-APP_DEBUG = bool(os.environ.get("DEBUG"))
-DEV_TOOLS_PROPS_CHECK = bool(os.environ.get("DEV_TOOLS_PROPS_CHECK"))
+# APP_HOST = os.environ.get("HOST")
+# APP_PORT = int(os.environ.get("PORT"))
+APP_DEBUG = True # bool(os.environ.get("DEBUG"))
+DEV_TOOLS_PROPS_CHECK = True # bool(os.environ.get("DEV_TOOLS_PROPS_CHECK"))
--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 from app import app, server
 from routes import render_page_content
 
-from environment.settings import APP_HOST, APP_PORT, APP_DEBUG, DEV_TOOLS_PROPS_CHECK
+from environment.settings import APP_DEBUG, DEV_TOOLS_PROPS_CHECK #, APP_HOST, APP_PORT
 
 
 if __name__ == "__main__":
     app.run_server(
-        host=APP_HOST,
-        port=APP_PORT,
+        #host=APP_HOST,
+        #port=APP_PORT,
         debug=APP_DEBUG,
         dev_tools_props_check=DEV_TOOLS_PROPS_CHECK
     )
